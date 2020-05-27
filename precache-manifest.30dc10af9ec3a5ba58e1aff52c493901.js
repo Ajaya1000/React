e8d75e43cba0cc86607930d76aa97fb713524a0d
@itsAj <ajaymati1000@gmail.com>
2020-05-27 13:51:02 +0530

    Updates

---
+++ precache-manifest.30dc10af9ec3a5ba58e1aff52c493901.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff39d337eabe97d4ace0ee242a6805a6",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "2a1e97ca7aaa149e911c",
+    "url": "/React/static/css/2.ad2a6cfd.chunk.css"
+  },
+  {
+    "revision": "b6ef9e00a5deb6d428a6",
+    "url": "/React/static/css/main.e3b8daca.chunk.css"
+  },
+  {
+    "revision": "2a1e97ca7aaa149e911c",
+    "url": "/React/static/js/2.a94605cf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React/static/js/2.a94605cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6ef9e00a5deb6d428a6",
+    "url": "/React/static/js/main.64bb9717.chunk.js"
+  },
+  {
+    "revision": "fb2dba3ae877868f0090",
+    "url": "/React/static/js/runtime-main.314dd5f8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
